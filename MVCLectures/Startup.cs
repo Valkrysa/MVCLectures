f4fe0cd125cb2fbc38121a732a01fc3cf96a3f4e
Alice Robinson <valkrysa@gmail.com>
2017-12-07 15:50:25 -0800

    Add project files.

---
+++ MVCLectures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLectures.Startup))]
+namespace MVCLectures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
